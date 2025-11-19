a5563989bf10a4dcd33e392224594137ef8615c1
sandrasavkovic <sandrasavkovic86@gmail.com>
2025-11-19 16:25:45 +0100

    manji ekrani

--- reparacija/src/Navigation/Navbar.js
+++ reparacija/src/Navigation/Navbar.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Logo from "../photos/ReparacijaLogo.png";
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-             <div className="nav-logo">
-                <Link to="/">
-                    <img src={Logo} alt="Logo" className="logo-image" />
-                </Link>
-            </div> 
 
-            <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About us</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
-    );
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="logo-image" />
+        </Link>
+      </div>
+
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li onClick={toggleMenu}><Link to="/">Home</Link></li>
+        <li onClick={toggleMenu}><Link to="/about">About us</Link></li>
+        <li onClick={toggleMenu}><Link to="/contact">Contact</Link></li>
+      </div>
+
+      {/* Hamburger dugme */}
+      <div className={`hamburger ${isOpen ? "toggle" : ""}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
